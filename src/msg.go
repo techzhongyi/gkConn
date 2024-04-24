264eba16e8dd16e24274c236362ecf805374bab4
cuizx <872349307@qq.com>
2024-04-24 15:23:12 +0800

    c

--- src/msg.go
+++ src/msg.go
@@ -5,12 +5,18 @@ package gkCore
 import (
 	"github.com/IBM/sarama"
 	log "github.com/sirupsen/logrus"
+	"strconv"
 )
 
 // Save2Kafka 将数据存储到kafka
 func Save2Kafka(factoryId, msg string) {
-	if factoryId == "" || factoryId == LoginErr {
+	if factoryId == "" || factoryId == LoginErr || msg == "" {
 		log.Error("save2Kafka.......factoryId=", factoryId)
+		return
+	}
+	head := msg[0:48]
+	dataLen, _ := strconv.ParseInt(head[len(head)-4:], 16, 64)
+	if dataLen == 0 {
 		return
 	}
 	kMsg := &sarama.ProducerMessage{
